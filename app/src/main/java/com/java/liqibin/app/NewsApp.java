227222c4cbb2bbbb0f5a8fbc622bd46616e05cd6
lightbluebokeh <lightbluebokeh@outlook.com>
2019-08-29 11:38:44 +0800

    add database

--- app/src/main/java/com/java/liqibin/app/NewsApp.java
+++ app/src/main/java/com/java/liqibin/app/NewsApp.java
@@ -1,9 +1,14 @@
 package com.java.liqibin.app;
 
 import android.app.Application;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.java.liqibin.util.DatabaseHelper;
 
 public class NewsApp extends Application {
     private static NewsApp app;
+
+    private static DatabaseHelper databaseHelper;
 
     public static synchronized NewsApp getApp() {
         return app;
@@ -13,5 +18,15 @@ public class NewsApp extends Application {
     public void onCreate() {
         super.onCreate();
         app = this;
+
+        databaseHelper = new DatabaseHelper(this, "news.db");
+    }
+
+    public SQLiteDatabase getWritableDatabase() {
+        return databaseHelper.getWritableDatabase();
+    }
+
+    public SQLiteDatabase getReadableDatabase() {
+        return databaseHelper.getReadableDatabase();
     }
 }
--- app/src/main/java/com/java/liqibin/util/DatabaseHelper.java
+++ app/src/main/java/com/java/liqibin/util/DatabaseHelper.java
@@ -1,4 +1,48 @@
 package com.java.liqibin.util;
 
-public class DatabaseHelper {
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final int VERSION = 1;
+    private static final String TABLE_NAME = "news";
+
+    public DatabaseHelper(@Nullable Context context, @Nullable String name) {
+        super(context, name, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "create table if not exists " + TABLE_NAME + " (" +
+                "id integer primary key, " +
+                "image text, " +
+                "publishTime text, " +
+                "keywords text, " +
+                "video text, " +
+                "title text, " +
+                "_when text, " +
+                "content text, " +
+                "persons text, " +
+                "newsID text, " +
+                "crawlTime text, " +
+                "organizations text, " +
+                "publisher text, " +
+                "locations text, " +
+                "_where text, " +
+                "category text," +
+                "who text);";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("drop table if exists " + TABLE_NAME);
+        onCreate(db);
+    }
+
 }
